d388d0afe18ebb3fe10b1d05175969eafd10d1d5
Vivi2901 <202000200@est.umss.edu>
2022-10-13 00:48:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// material-ui
+
+import {
+    Button,
+} from '@mui/material';
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
 
 function App() {
-  return (
+
+   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>criss</h1>
+      <Button color="primary">Hello World</Button>
+      
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
       </header>
     </div>
-  );
-}
+  );  
+  
+  }
 
 export default App;
