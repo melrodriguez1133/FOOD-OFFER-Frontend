3ffe84d0a579426e58309b1c0985d3822dfe8faa
QTMChris <202000653@est.umss.edu>
2022-10-15 06:58:11 -0400

    Formulario, Fecha_hora

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {DatePicker, TimePicker, DateTimePicker} from '@material-ui/pickers';
+import {Formulario, Label, GrupoInput, Input} from './elementos/Formularios';
+import {FontAwesomeIcon} from'@fortawesome/react-fontawesome';
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+/*import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';*/
+//import Datetime from 'react-datetime';
+
 
 function App() {
+  const [nombre,setNombre]=useState("Nombre de la empresa");
+
+  /*const cambiarNombre=(e)=>{
+    const value=e.target.value;
+    console.log(value);
+    setNombre(value);
+  };
+
+  const Formulario = styled.form`
+    background: #ccc;
+  `;*/
+
+  const [fechaSeleccionada, cambiarFechaSeleccionada] = useState(new Date());
+  console.log(fechaSeleccionada)
+
+  const guardarClick=()=>{
+  
+  console.log("Esta es mi estado local: ",nombre);
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <main>
+          <Formulario action=''>
+            <div>
+              <Label htmlFor=''>Registro de Empresa</Label>
+              <GrupoInput>
+                <Input type="text" placeholder='Empresa'/>
+                <FontAwesomeIcon icon={faCheckCircle}/>
+              </GrupoInput>
+              
+            </div>
+            <div>
+              <Label htmlFor=''>Tipo de Negocio</Label>
+              <Input type="text" placeholder='Tipo'/>
+            </div>
+      </Formulario>
+      <br></br>
+      <Formulario action=''>
+            
+      </Formulario>
+      <br></br>
+      <form action=''>
+            <label htmlFor=''>Descripción</label>
+            <input type="text" placeholder='Descripcón de la empresa'/>
+      </form>
+      <br/>
+       <div className='Fecha_y_Hora'>
+          <label className="label">Horarios de Trabajo </label>
+            <DateTimePicker value={fechaSeleccionada} onChange={cambiarFechaSeleccionada} />
+       </div>
+       </main>  
+/*
+<button onClick={guardarClick} className="boton">Guardar</button>  
+
+<label className="label">Nombre </label>
+          <i class="fa-thin fa-user" aria-hidden="true"/>
+          <input 
+            id="nombre" 
+            name="nombre" 
+            value={nombre} 
+            onChange={cambiarNombre}
+            className="input"
+        ></input>
+
+        <label className="label">Descripción </label>
+        <i class="fa-thin fa-user" aria-hidden="true"/>
+          <input 
+            id="nombre" 
+            name="nombre" 
+            value={nombre} 
+            onChange={cambiarNombre}
+            className="input"
+        ></input>
+
+<div className='Fecha'>
+          <label>Fecha </label>
+            <DatePicker value={fechaSeleccionada} onChange={cambiarFechaSeleccionada} />
+       </div>
+       <div className='Hora'>
+          <label>Hora </label>
+            <TimePicker value={fechaSeleccionada} onChange={cambiarFechaSeleccionada} />
+       </div>*/
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import esLocale from 'date-fns/locale/es';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={esLocale}>
+      <App />
+    </MuiPickersUtilsProvider>
   </React.StrictMode>
 );
 
