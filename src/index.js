e2be6371d668bbd2cccb4c9a92b22f071d42d015
QTMChris <202000653@est.umss.edu>
2022-10-18 02:22:27 -0400

    Prueba

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -14,4 +14,81 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
+import ReactDOM from 'react-dom';
+import ImageResponsive from 'react-image-responsive';
+ 
+const sources = [
+  { maxWidth: 100, src: 'http://placehold.it/100x100' },
+  { maxWidth: 200, src: 'http://placehold.it/200x100' },
+  { maxWidth: 400, src: 'http://placehold.it/400x100' },
+  { maxWidth: 800, src: 'http://placehold.it/800x100' },
+  { maxWidth: 1200, src: 'http://placehold.it/1200x100' }
+];
+ 
+ReactDOM.render(
+  <div className="App">
+    <figure>
+      <ImageResponsive
+        src="http://placehold.it/50x50"
+        alt="this is my image"
+        width="100px"
+        height="100px"
+        sources={sources}
+      />
+      <figcaption>
+        This image has a fixed width of 100px, you should see a 100px image
+      </figcaption>
+    </figure>
+    <figure>
+      <ImageResponsive
+        src="http://placehold.it/50x50"
+        alt="this is my image"
+        width="105px"
+        height="100px"
+        sources={sources}
+      />
+      <figcaption>
+        This image has a fixed width of 105px, you should see a 200px image
+      </figcaption>
+    </figure>
+ 
+    <figure>
+      <ImageResponsive
+        src="http://placehold.it/50x50"
+        alt="this is my image"
+        width="400px"
+        height="100px"
+        sources={sources}
+      />
+      <figcaption>
+        This image has a fixed width of 400px, you should see a 400px image
+      </figcaption>
+    </figure>
+    <figure>
+      <ImageResponsive
+        src="http://placehold.it/50x50"
+        alt="this is my image"
+        width="20%"
+        height="100px"
+        sources={sources}
+      />
+      <figcaption>
+        This image has a 20% width, resize your browser to see the src adjusting
+      </figcaption>
+    </figure>
+    <figure>
+      <ImageResponsive
+        src="http://placehold.it/50x50"
+        alt="this is my image"
+        width="50%"
+        height="100px"
+        sources={sources}
+      />
+      <figcaption>
+        This image has a 50% width, resize your browser to see the src adjusting
+      </figcaption>
+    </figure>
+  </div>,
+  document.getElementById('root')
+);
