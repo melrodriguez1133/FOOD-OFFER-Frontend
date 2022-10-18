8fa63671c271cc57d35e30c4c67fd2af047b5311
QTMChris <202000653@est.umss.edu>
2022-10-18 00:46:47 -0400

    Validacion de inputs

--- src/App.js
+++ src/App.js
@@ -22,6 +22,23 @@ import Button from '@mui/material/Button';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { LoadingButton } from '@mui/lab';
+import { Grid, Card, CardContent  } from '@mui/material';
+
+const dataEspecialist = [
+  {
+    id: 1,
+    descriptions: 'Cafeteria',
+  },
+  {
+    id: 2,
+    descriptions: 'Supermercado',
+  },
+  {
+    id: 3,
+    descriptions: 'Restaurante',
+  }
+]
 
 function App() {
   const [nombre,setNombre]=useState("Nombre de la empresa");
@@ -36,7 +53,47 @@ function App() {
         setValue(newValue);
       };
 
+      const dataEspecialist = [
+        {
+          id: 1,
+          descriptions: 'Cafeteria',
+        },
+        {
+          id: 2,
+          descriptions: 'Supermercado',
+        },
+        {
+          id: 3,
+          descriptions: 'Restaurante',
+        }
+      ]
 
+      const [loading, setLoading] = useState(false);
+      const [formData, setFormData] = useState({
+        nameEmpresa: '',
+        descripcion:'',
+        horaIni:'',
+        horaFin:'',
+        lastname: '',
+        rol: 0
+    
+      });
+    
+      const { nameEmpresa, descripcion,horaIni,horaFin,lastname, rol } = formData;
+    
+      const handleOnchange = (e) => {
+        console.log([e.target.name], e.target.value)
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      }
+    
+      const handleSubmit = (nameEmpresa, descripcion,horaIni,horaFin,lastname, rol) => {
+        console.log(nameEmpresa,descripcion, horaIni,horaFin,lastname, rol);
+        setLoading(true)
+        alert(`datos formularios:::, ${nameEmpresa}, ${descripcion},${horaIni},${horaFin},${lastname}, ${rol}`)
+        setTimeout(() => {
+          setLoading(false)
+        }, 3000);
+      }
 
   return (
     
@@ -46,36 +103,59 @@ function App() {
               <Label htmlFor=''>Registro de Empresa</Label>
               
             <TextField
-          id="outlined-multiline-static"
+
+                    required
+                    error={false}
+                    label="Nombre de la empresa"
+                    type="text"
+                    name="nameEmpresa"
+                    value={nameEmpresa}
+                    onChange={handleOnchange}
+                    margin="dense"
+                    fullWidth
+                    variant="outlined"
+                  
+
+         /* id="outlined-multiline-static"
           label="Nombre de la Empresa"
-          variant="outlined"
+          variant="outlined"*/
           />
             <br></br>
               <br></br>
-              <Box sx={{ minWidth: 120 }}>
-          <FormControl variant="outlined">
-            <InputLabel id="demo-simple-select-label">Tipo de Negocio</InputLabel>
-            <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={tipo}
-            label="Tipo de Negocio"
-            onChange={handleChange}
-            >
-            <MenuItem value={10}>Cafeteria</MenuItem>
-            <MenuItem value={20}>Comercial</MenuItem>
-            <MenuItem value={30}>Restaurantes</MenuItem>
-            </Select>
+              
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <FormControl sx={{ width: '100%' }}>
+                  <InputLabel id="demo-simple-select-label">Tipo de empresa</InputLabel>
+              <Select
+                    required
+                      name="tipoEmpresa"
+                      value={rol}
+                      fullWidth
+                      label="tipoEmpresa"
+                      onChange={handleOnchange}>
+                      <MenuItem value={0}>Seleccione</MenuItem>
+                      {dataEspecialist && dataEspecialist.map((d, index) => (
+                        <MenuItem key={d.id} value={d.id}>{d.descriptions}</MenuItem>
+                      ))}
+                      </Select>
           </FormControl>
-          </Box>
+          </Grid>
           
           <br></br>
           <TextField
-          id="outlined-multiline-static"
-          label="Descripcion"
-          multiline
-          rows={5}
-          variant="outlined"
+
+                required
+                  error={false}
+                  label="Descripcion"
+                  multiline
+                  rows={5}
+                  type="text"
+                  name="descripcion"
+                  value={descripcion}
+                  onChange={handleOnchange}
+                  margin="dense"
+                  fullWidth
+                  variant="outlined"
           />
 
         <br></br>
@@ -96,29 +176,40 @@ function App() {
             </div>
             <FormControlLabel control={<Checkbox defaultChecked />} label="Domingo" />
           </FormGroup>
-        
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Stack spacing={3}>
-            <TimePicker
-              Label="Hora de Apertura"
-              value={value}
-              onChange={handleChange1}
-              renderInput= {<TextField id="outlined-multiline-static" label="Número de Celular" variant="outlined" />}
-            />
-            <TimePicker
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          
+            <TextField
+            required
+              id="time"
               label="Hora de Cierre"
-              value={value}
+              type="time"
+              name="horaFin"
+              value={horaFin}
               onChange={handleChange1}
-              renderInput= {<TextField
-          id="outlined-multiline-static"
-          label="Correo de Empresa"
-          variant="outlined"
-          />}
+              margin="dense"
+                  variant="outlined"
+                  fullWidth
             />
-            </Stack>
-            </LocalizationProvider>
+            
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            
+            <TextField
+            required
+              id="time"
+              label="Hora de Cierre"
+              type="time"
+              name="horaFin"
+              value={horaFin}
+              onChange={handleChange1}
+              margin="dense"
+                  variant="outlined"
+                  fullWidth
+            />
+            
+            </Grid>
             </div>
-        
+                
             <div>
             <br></br>
             <br></br>
@@ -156,7 +247,21 @@ function App() {
             
             
             </div>
-            <Button variant="primary" type="submit">
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Box sx={{ '& > button': { m: 1 } }}>
+                    <LoadingButton
+                      size="medium"
+                      onClick={() => handleSubmit(nameEmpresa,descripcion,horaIni, lastname, rol)}
+                      loading={loading}
+                      variant="outlined"
+                      disabled={!loading ? false : true}
+                    >
+                      Enviar
+                    </LoadingButton>
+
+                  </Box>
+                </Grid>
+                <Button variant="primary" type="submit">
               Registrar
             </Button>
             <Button variant="primary" type="submit">
