8694e8687403764a6bc4c5f6bb67ea18cf7c660d
Your Name <karion.praus.03@gmail.com>
2022-10-19 01:44:23 -0400

    "vista"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import imagfoodre from './imagfoodre.png';
 import './App.css';
-
 function App() {
   return (
   <header>
     <body>
+    <div id="foodOffer">Food Offer</div>
+    <div id="titulo">Registro de Empresa</div>
       <div id="imagComida">
       <img src={imagfoodre} class="imag" alt="logo" />
       </div>
       <div id="moveform">
           <lightning-card  id="tamform">
+            <h1>melani</h1>
             <form>
 
 
