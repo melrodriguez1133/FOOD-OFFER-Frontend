295196bb627f31034e3a801d838aad4b142c2581
melrodriguez1133 <202000231@est.umss.edu>
2022-11-03 01:02:43 -0400

    "label bases"

--- src/App.js
+++ src/App.js
@@ -5,56 +5,41 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import Input from './componentes/Input';
 
 const App = () => {
-	const [usuario, cambiarUsuario] = useState({campo: '', valido: null});
 	const [nombre, cambiarNombre] = useState({campo: '', valido: null});
+	const [descripcion, cambiarDescripcion] = useState({campo: '', valido: null});
+	const [fechaVencimiento, cambiarFechaVencimento] = useState({campo: '', valido: null});
 	const [password, cambiarPassword] = useState({campo: '', valido: null});
 	const [password2, cambiarPassword2] = useState({campo: '', valido: null});
 	const [correo, cambiarCorreo] = useState({campo: '', valido: null});
 	const [telefono, cambiarTelefono] = useState({campo: '', valido: null});
-	const [terminos, cambiarTerminos] = useState(false);
 	const [formularioValido, cambiarFormularioValido] = useState(null);
 
 	const expresiones = {
-		usuario: /^[a-zA-Z0-9_-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-		nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+		nombre: /^[a-zA-ZÀ-ÿ\s]{4,16}$/, // Letras y espacios, pueden llevar acentos.
+		descripcion: /^[a-zA-ZÀ-ÿ\s]{25,250}$/, // Letras y espacios, pueden llevar acentos.
+		fechaVencimiento:/([0-2][0-9]|3[0-1])(\/|-)(){25,250}$/,// 7 a 14 numeros.
 		password: /^.{4,12}$/, // 4 a 12 digitos.
 		correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 		telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 	}
 
-	const validarPassword2 = () => {
-		if(password.campo.length > 0){
-			if(password.campo !== password2.campo){
-				cambiarPassword2((prevState) => {
-					return {...prevState, valido: 'false'}
-				});
-			} else {
-				cambiarPassword2((prevState) => {
-					return {...prevState, valido: 'true'}
-				});
-			}
-		}
-	}
-
-	const onChangeTerminos = (e) => {
-		cambiarTerminos(e.target.checked);
-	}
 
 	const onSubmit = (e) => {
 		e.preventDefault();
 
 		if(
-			usuario.valido === 'true' &&
 			nombre.valido === 'true' &&
+			descripcion.valido === 'true' &&
+			fechaVencimiento.valido === 'true' &&
 			password.valido === 'true' &&
 			password2.valido === 'true' &&
 			correo.valido === 'true' &&
-			telefono.valido === 'true' &&
-			terminos
+			telefono.valido === 'true' 
 		){
 			cambiarFormularioValido(true);
-			cambiarUsuario({campo: '', valido: ''});
 			cambiarNombre({campo: '', valido: null});
+			cambiarDescripcion({campo: '', valido: null});
+			cambiarFechaVencimento({campo: '', valido: null});
 			cambiarPassword({campo: '', valido: null});
 			cambiarPassword2({campo: '', valido: 'null'});
 			cambiarCorreo({campo: '', valido: null});
@@ -70,16 +55,6 @@ const App = () => {
 		<main>
 			<Formulario action="" onSubmit={onSubmit}>
 				<Input
-					estado={usuario}
-					cambiarEstado={cambiarUsuario}
-					tipo="text"
-					label="Usuario*"
-					placeholder="john123"
-					name="usuario"
-					leyendaError="El usuario tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo."
-					expresionRegular={expresiones.usuario}
-				/>
-				<Input
 					estado={nombre}
 					cambiarEstado={cambiarNombre}
 					tipo="text"
@@ -90,45 +65,65 @@ const App = () => {
 					expresionRegular={expresiones.nombre}
 				/>
 				<Input
-					estado={password}
-					cambiarEstado={cambiarPassword}
-					tipo="password"
-					label="Contraseña*"
-					name="password1"
-					leyendaError="La contraseña tiene que ser de 4 a 12 dígitos."
-					expresionRegular={expresiones.password}
+					estado={descripcion}
+					cambiarEstado={cambiarDescripcion}
+					tipo="text"
+					label="Descripcion*"
+					placeholder="inserte una descripcion"
+					name="descripcion"
+					leyendaError="El nombre solo puede contener letras y espacios."
+					expresionRegular={expresiones.descripcion}
 				/>
 				<Input
-					estado={password2}
-					cambiarEstado={cambiarPassword2}
-					tipo="password"
-					label="Repetir Contraseña*"
-					name="password2"
-					leyendaError="Ambas contraseñas deben ser iguales."
-					funcion={validarPassword2}
+					estado={fechaVencimiento}
+					cambiarEstado={cambiarFechaVencimento}
+					tipo="date"
+					label="Fecha de Vencimiento*"
+					name="fechaVencimiento"
+					leyendaError="solo debe ingresar una fecha"
+					expresionRegular={expresiones.fechaVencimiento}
 				/>
 				<Input
-					estado={correo}
-					cambiarEstado={cambiarCorreo}
-					tipo="email"
-					label="Correo Electrónico*"
-					placeholder="john@correo.com"
-					name="correo"
-					leyendaError="El correo solo puede contener letras, numeros, puntos, guiones y guion bajo."
-					expresionRegular={expresiones.correo}
+					estado={nombre}
+					cambiarEstado={cambiarNombre}
+					tipo="date"
+					label="Fecha de Elaboracion*"
+					placeholder="John Doe"
+					name="usuario"
+					leyendaError="El nombre solo puede contener letras y espacios."
+					expresionRegular={expresiones.nombre}
 				/>
 				<Input
 					estado={telefono}
 					cambiarEstado={cambiarTelefono}
 					tipo="text"
-					label="Teléfono*"
+					label="Precio*"
 					placeholder="4491234567"
 					name="telefono"
 					leyendaError="El telefono solo puede contener numeros y el maximo son 14 dígitos."
 					expresionRegular={expresiones.telefono}
 				/>
-
-
+				<Input
+					estado={telefono}
+					cambiarEstado={cambiarTelefono}
+					tipo="text"
+					label="Cantidad disponible*"
+					placeholder="4491234567"
+					name="telefono"
+					leyendaError="El telefono solo puede contener numeros y el maximo son 14 dígitos."
+					expresionRegular={expresiones.telefono}
+				/>
+				<Input
+					estado={nombre}
+					cambiarEstado={cambiarNombre}
+					tipo="date"
+					label="Fecha Limite de Oferta*"
+					placeholder="John Doe"
+					name="usuario"
+					leyendaError="El nombre solo puede contener letras y espacios."
+					expresionRegular={expresiones.nombre}
+				/>
+				
 
 				
 				{formularioValido === false && <MensajeError>
