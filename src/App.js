880fb1818352289ab2afcbb60ac7c9809d65dfed
melrodriguez1133 <202000231@est.umss.edu>
2022-11-04 14:20:11 -0400

    validaciones casi completas

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import {Formulario, ContenedorBotonCentrado, Boton, MensajeExito, MensajeError} 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import Input from './componentes/Input';
+import axios from 'axios';
+
 
 const App = () => {
+    
+	
+
 	const [nombre, cambiarNombre] = useState({campo: '', valido: null});
 	const [descripcion, cambiarDescripcion] = useState({campo: '', valido: null});
 	const [fechaVencimiento, cambiarFechaVencimento] = useState({campo: '', valido: null});
@@ -17,11 +22,11 @@ const App = () => {
 	const expresiones = {
 		nombre: /^[a-zA-ZÀ-ÿ0-9\s]{4,20}$/, // Letras y espacios, pueden llevar acentos.
 		descripcion: /^[a-zA-ZÀ-ÿ0-9\s]{25,250}$/, // Letras y espacios, pueden llevar acentos.
-		//fechaVencimiento:/^\d{1,2}\/\d{1,2}\/\d{2,4}$/,// 7 a 14 numeros.
-		//fechaElaboracion: /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/,// 7 a 14 numeros.
+		fechaVencimiento: /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/,//dd/mm/aaaa
+		fechaElaboracion: /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/,//dd/mm/aaaa
 		precio: /^[0-9Bs\s]{3,20}$/, // 4 a 12 digitos.
 		cantidadDisponible: /^[0-9]{1,3}$/, // 7 a 14 numeros.
-		//fechaLimite:/^\d{1,2}\/\d{1,2}\/\d{2,4}$/,// 7 a 14 numeros.
+		fechaLimite: /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/,//dd/mm/aaaa
 	}
 
 
@@ -51,16 +56,18 @@ const App = () => {
 			cambiarFormularioValido(false);
 		}
 	}
+	
 
 	return (
 		<main>
+			<h1>Registrar Productos</h1>
 			<Formulario action="" onSubmit={onSubmit}>
 				<Input
 					estado={nombre}
 					cambiarEstado={cambiarNombre}
 					tipo="text"
 					label="Nombre*"
-					placeholder="John Doe"
+					placeholder="Ingrese el nombre del producto"
 					name="usuario"
 					leyendaError="El nombre solo puede contener datos alfanumericos y espacios"
 					expresionRegular={expresiones.nombre}
@@ -78,19 +85,21 @@ const App = () => {
 				<Input
 					estado={fechaVencimiento}
 					cambiarEstado={cambiarFechaVencimento}
-					tipo="date"
+					tipo="text"
 					label="Fecha de Vencimiento*"
+					placeholder="ingrese dd/mm/aaaa o dd-mm-aaaa"
 					name="fechaVencimiento"
-					leyendaError="solo es valido un dato tipo fecha dd/mm/aaaa"
+					leyendaError="solo es valido un dato tipo fecha dd/mm/aaaa dd-mm-aaaa"
 					expresionRegular={expresiones.fechaVencimiento}
 				/>
 				<Input
 					estado={fechaElaboracion}
 					cambiarEstado={cambiarFechaElaboracion}
-					tipo="date"
+					tipo="text"
 					label="Fecha de Elaboracion*"
+					placeholder="ingrese dd/mm/aaaa o dd-mm-aaaa"
 					name="fechaElaboracion"
-					leyendaError="solo es valido un dato tipo fecha dd/mm/aaaa"
+					leyendaError="solo es valido un dato tipo fecha dd/mm/aaaa o dd-mm-aaaa"
 					expresionRegular={expresiones.fechaElaboracion}
 				/>
 				<Input
@@ -116,15 +125,16 @@ const App = () => {
 				<Input
 					estado={fechaLimite}
 					cambiarEstado={cambiarFechaLimite}
-					tipo="date"
+					tipo="text"
 					label="Fecha Limite de Oferta*"
+					placeholder="ingrese dd/mm/aaaa o dd-mm-aaaa"
 					name="fechaLimite"
-					leyendaError="solo es valido un dato tipo fecha dd/mm/aaaa"
+					leyendaError="solo es valido un dato tipo fecha dd/mm/aaaa o dd-mm-aaaa"
 					expresionRegular={expresiones.fechaLimite}
 				/>
 				
 
-				
+				<br/>
 				{formularioValido === false && <MensajeError>
 					<p>
 						<FontAwesomeIcon icon={faExclamationTriangle}/>
