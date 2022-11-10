be2d852b87aeadbee217fafc1535c32690933dd2
melrodriguez1133 <202000231@est.umss.edu>
2022-11-09 23:56:27 -0400

    arreglado css

--- src/Pages/CreateRegistrarEmpresa.js
+++ src/Pages/CreateRegistrarEmpresa.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
-import './CreateRegistrarEmpresa.css';
+import './EditRegistrarEmpresa.css';
 import {Link} from "react-router-dom"
 
 const endpoint = 'http://localhost:8000/api/registrarEmpresas'
