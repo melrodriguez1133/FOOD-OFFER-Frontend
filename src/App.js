bb8cab420a770d1b14e4d0b1960910675fe2a217
melrodriguez1133 <202000231@est.umss.edu>
2022-11-18 00:24:55 -0400

    modelo de login

--- src/App.js
+++ src/App.js
@@ -9,9 +9,114 @@ import RegistroProducto from './Pages/RegistroProducto';
 import EditarProductos from './Pages/EditarProductos';
 import EditRegistrarEmpresa from './Pages/EditRegistrarEmpresa'
 
-function App() {
-  return (
-    <Router>
+import React, { useState } from 'react'
+import { Grid, Container, Paper, Avatar, Typography, TextField, Button, CssBaseline } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import fondo from './assets/images/fondo.png'
+import { LockOutlined as LockOutlinedIcon } from '@material-ui/icons';
+
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		backgroundImage: `url(${fondo})`,
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+		height: '100vh'
+	},
+	container: {
+		opacity: '0.8',
+		height: '60%',
+		marginTop: theme.spacing(10),
+		[theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
+			marginTop: 0,
+			width: '100%',
+			height: '100%'
+		}
+	},
+	div: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.primary.main
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(1)
+	},
+	button: {
+		margin: theme.spacing(3, 0, 2)
+	}
+}))
+
+const App = () => {
+	const [body, setBody] = useState({ nickname: '', password: '' })
+	const classes = useStyles()
+
+
+	const handleChange = e => {
+		setBody({
+			...body,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const onSubmit = () => {
+		console.log(body)
+	}
+
+	return (
+		<Grid container component='main' className={classes.root}>
+			<CssBaseline />
+			<Container component={Paper} elevation={5} maxWidth='xs' className={classes.container}>
+				<div className={classes.div}>
+					<Avatar className={classes.avatar}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>Sign In</Typography>
+					<form className={classes.form}>
+						<TextField
+							fullWidth
+							autoFocus
+							color='primary'
+							margin='normal'
+							variant='outlined'
+							label='Nickname'
+							name='nickname'
+							value={body.nickname}
+							onChange={handleChange}
+						/>
+						<TextField
+							fullWidth
+							type='password'
+							color='primary'
+							margin='normal'
+							variant='outlined'
+							label='Password'
+							name='password'
+							value={body.password}
+							onChange={handleChange}
+						/>
+						<Button
+							fullWidth
+							variant='contained'
+							color='secondary'
+							className={classes.button}
+							onClick={() => onSubmit()}
+						>
+							Sign In
+						</Button>
+					</form>
+				</div>
+			</Container>
+		</Grid>
+	
+
+    /*<Router>
       <Sidebar />
       <Routes>
         <Route path='/empresa/empresa' element={<ShowRegistrarEmpresas />} />
@@ -24,7 +129,7 @@ function App() {
         <Route path='/empresa/productos-empresa/EditarProducto/:id' element={ <EditarProductos/>}/>
         <Route path='/' element={<VistaProductos />} />
       </Routes>
-    </Router>
+    </Router>**/
    
   );
 }        
