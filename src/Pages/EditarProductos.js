8b25986bf3d8f7cc1f99a524e1bd46c49ca6b2b1
Chris <115061294+QTMChris@users.noreply.github.com>
2022-11-21 13:55:52 -0400

    "EditarProducto 2.1"

--- src/Pages/EditarProductos.js
+++ src/Pages/EditarProductos.js
@@ -17,9 +17,9 @@ import { IconName } from "react-icons/io5";
 
 const EditProduct = () => {
    
-	const [products]=useFetch("http://127.0.0.1:8000/api/registrarEmpresas");
+	const [products]=useFetch("https://isbackend-production.up.railway.app/api/Categoria");
 	//console.log(products);
-    const endpoint = 'http://localhost:8000/api/Producto/'
+    const endpoint = 'https://isbackend-production.up.railway.app/api/Producto'
 
 	const [nombre, cambiarNombre] = useState({campo: '', valido: null});
 	const [descripcion, cambiarDescripcion] = useState({campo: '', valido: null});
@@ -94,7 +94,7 @@ const EditProduct = () => {
     data.append('file',Imag.file, Imag.name);
 	data.append('product', JSON.stringify(inputsT));
     
-    fetch('http://127.0.0.1:8000/api/Producto', {
+    fetch('https://isbackend-production.up.railway.app/api/Producto', {
 				method: "POST",
 				body: data
 			})
